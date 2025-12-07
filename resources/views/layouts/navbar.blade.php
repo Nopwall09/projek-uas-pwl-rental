a02f478c35ccab2a236c3591a0fa434779e6807f
Syakir003 <syakirdaulay445@gmail.com>
2025-12-07 20:49:14 +0700

    nambah navbar

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <nav class="navbar">
+        <div class="logo">
+            <img src="img/logo.png" alt="Logo">
+        </div>
+
+        <div class="hamburger" onclick="toggleMenu()">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+
+        <ul class="nav-menu" id="menu">
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">KATALOG MOBIL</a></li>
+            <li><a href="#">KONTAK</a></li>
+            <li><a href="#">LOGIN</a></li>
+        </ul>
+    </nav>
+</header>
