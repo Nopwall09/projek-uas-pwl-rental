36eb503bfe78723afed16201a6878fd1bc8e705e
Muhammad Sumbul <109408823+Nopwall09@users.noreply.github.com>
2025-12-14 15:11:37 +0700

    add resources

---
+++ app/Filament/Admin/Resources/Mobils/Tables/MobilsTable.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Filament\Admin\Resources\Mobils\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+
+class MobilsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
