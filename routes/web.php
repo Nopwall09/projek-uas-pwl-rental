eea28f8bbeb114d4acdb2b8e2f78c90e49c0ef71
Muhammad Sumbul <109408823+Nopwall09@users.noreply.github.com>
2025-12-15 06:26:23 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -99,7 +99,8 @@ Route::middleware(['kasir'])->group(function () {
     Route::get('/kasir/dashboard', [RentalItemController::class, 'dashboard'])
         ->name('kasir.dashboard')
         ->middleware('kasir');
-
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
     //Buat CREATE
     Route::get('/kasir/create', [RentalItemController::class, 'create'])
         ->name('kasir.create');
